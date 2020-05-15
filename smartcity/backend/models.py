bc112c5d9b9ec1023e83b1901e0dd5fb00bc4199
ardesp <belloshehu1@gmail.com>
2020-05-15 16:48:28 +0100

    ServiceProvider model added

--- smartcity/backend/models.py
+++ smartcity/backend/models.py
@@ -49,3 +49,25 @@ class LocalGovernmentArea(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+    
+class ServiceProvider(models.Model):
+    """Model for service providers"""
+    name = models.CharField(max_length=50)
+    address1 = models.CharField(max_length=100)
+    address2 = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=100)
+    service_rendered = models.CharField(max_length=50)
+    service_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=50)
+    description = models.TextField(max_length=200)
+    year_of_experience = models.IntegerField()
+    year_of_establishement = models.DateField(default=timezone.now().date())
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+    local_government_are = models.ForeignKey(LocalGovernmentArea, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    supporting_document = models.FileField()
+    rating = models.FloatField()
+
+    def __str__(self):
+        return f'{self.name}'
