91e2d15e9a62903357413cfcf5e9fc0445f99af0
ardesp <belloshehu1@gmail.com>
2020-05-15 16:14:12 +0100

    Country model added

--- smartcity/backend/models.py
+++ smartcity/backend/models.py
@@ -18,9 +18,17 @@ class Service(models.Model):
 
 class Category(models.Model):
     """ Model for service categories"""
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     services = models.ForeignKey(Service, on_delete=models.CASCADE)
     document_required = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Country(models.Model):
+    """ Model for supported countries"""
+    name = models.CharField(max_length=50)
+    continent = models.CharField(choices=CONTINENTS, max_length=50)
 
     def __str__(self):
         return f'{self.name}'
