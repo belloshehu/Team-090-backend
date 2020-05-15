d0825df16c73cc0070626bf8a838075f24f842ab
ardesp <belloshehu1@gmail.com>
2020-05-15 16:08:23 +0100

    Service model added

--- smartcity/backend/models.py
+++ smartcity/backend/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+rom django.utils import timezone
+
+CONTINENTS = [
+    ('Africa','Africa'),
+    ('Europe','Europe'),
+    ('Asia','Asia'),
+]
 # Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.name}'
